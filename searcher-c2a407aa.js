ba1015aafc744502ab95111868cdce65adf54207
Cache McClure <cache.mcclure@gmail.com>
2026-01-11 00:13:42 -0600

    Deploy docs 2026-01-11

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d99b982a.js',
+                path_to_root + 'searchindex-0d6a328a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
